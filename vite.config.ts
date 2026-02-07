04eef76551d788bc79354a21c3842d446319f005
i-kipish <b.a.belova03@gmail.com>
2026-02-07 17:18:46 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/AI-Maturity-Index/",
+});
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
